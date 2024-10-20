7584bd52948b1d2d6064fede556fd0d3a91ec8ca
Atiq Samtia <hi@atiq.dev>
2024-10-21 01:42:07 +0500

    add High-Performance order storage support

--- woocommerce-rewardful.php
+++ woocommerce-rewardful.php
@@ -71,3 +71,9 @@ function run_woocommerce_rewardful() {
 
 }
 run_woocommerce_rewardful();
+
+add_action( 'before_woocommerce_init', function() {
+    if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+        \Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+    }
+} );
